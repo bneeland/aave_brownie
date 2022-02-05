bf5da5c434c5b85a3022a5858360cd56ee692d47
Brian Neeland <brian.neeland@protonmail.com>
2022-02-05 11:18:26 -0700

    Approving ERC20 contract

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,22 +1,34 @@
 from brownie import network, config, interface
 from scripts.helpful_scripts import get_account
 from scripts.get_weth import get_weth
+from web3 import Web3
+
+amount = Web3.toWei(0.1, "ether")
 
 def main():
     account = get_account()
     erc20_address = config["networks"][network.show_active()]["weth_token"]
     if network.show_active() in ["mainnet-fork"]:
         get_weth()
-    # Need ABI and Address
     lending_pool = get_lending_pool()
-    print(lending_pool)
+    # Approve sending out ERC20 tokens
+    approve_erc20(amount, lending_pool.address, erc20_address, account)
+
+def approve_erc20(amount, spender, erc20_address, account):
+    # Need ABI and address
+    print("Approving ERC20 token...")
+    erc20 = interface.IERC20(erc20_address)
+    tx = erc20.approve(spender, amount, {"from": account})
+    tx.wait(1)
+    print("Approved")
+    return tx
+
 
 def get_lending_pool():
-    # Get address
+    # Need ABI and address
     lending_pool_addresses_provider = interface.ILendingPoolAddressesProvider(
         config["networks"][network.show_active()]["lending_pool_addresses_provider"]
     )
     lending_pool_address = lending_pool_addresses_provider.getLendingPool()
-    # Get ABI
     lending_pool = interface.ILendingPool(lending_pool_address)
     return lending_pool
