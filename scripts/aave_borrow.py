f09a921a5839c080e43cba513cabc4dede8cd51e
Brian Neeland <brian.neeland@protonmail.com>
2022-02-05 11:53:19 -0700

    Depositing and getting borrowed and borrowable amounts

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -13,6 +13,28 @@ def main():
     lending_pool = get_lending_pool()
     # Approve sending out ERC20 tokens
     approve_erc20(amount, lending_pool.address, erc20_address, account)
+    print("Depositing...")
+    tx = lending_pool.deposit(erc20_address, amount, account.address, 0, {"from": account}) # Referral code is depracated, just use 0 value
+    tx.wait(1)
+    print("Deposited")
+    # Determine amount to borrow
+    borrowable_eth, total_debt = get_borrowable_data(lending_pool, account)
+
+def get_borrowable_data(lending_pool, account):
+    (
+        total_collateral_eth,
+        total_debt_eth,
+        available_borrow_eth,
+        current_liquidation_threshold, ltv,
+        health_factor,
+    ) = lending_pool.getUserAccountData(account.address)
+    total_collateral_eth = Web3.fromWei(total_collateral_eth, "ether")
+    total_debt_eth = Web3.fromWei(total_debt_eth, "ether")
+    available_borrow_eth = Web3.fromWei(available_borrow_eth, "ether")
+    print(f"You have {total_collateral_eth} worth of eth deposited")
+    print(f"You have {total_debt_eth} worth of eth borrowed")
+    print(f"You can borrow {available_borrow_eth} worth of eth deposited")
+    return (float(available_borrow_eth), float(total_debt_eth))
 
 def approve_erc20(amount, spender, erc20_address, account):
     # Need ABI and address
