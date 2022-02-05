a1d444250d37e7bfb1f58c7e1d5659147c7e972f
Brian Neeland <brian.neeland@protonmail.com>
2022-02-05 07:54:46 -0700

    Getting weth (step 1)

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,4 +1,12 @@
-
+from brownie import network, config
+from scripts.helpful_scripts import get_account
+from scripts.get_weth import get_weth
 
 def main():
-    pass
+    account = get_account()
+    erc20_address = config["networks"][network.show_active()]["weth_token"]
+    if network.show_active() in ["mainnet-fork"]:
+        get_weth()
+
+# https://www.youtube.com/watch?v=M576WGiDBdQ&t=23238s
+# 9:03:43
